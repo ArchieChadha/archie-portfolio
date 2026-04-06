5efbe288bd8e2d9c6a837cce023c49e0c894bcee
Archie Chadha <167891269+ArchieChadha@users.noreply.github.com>
2026-04-06 15:22:05 +1000

    Update Intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -36,7 +36,7 @@ class Intro extends React.Component {
         <FadeInSection>
           <div className="intro-subtitle">Welcome to my Digital Space!</div>
           <div className="intro-desc">
-            I'm a Software Developer from Melbourne, Australia with a knack for creating innovative solutions that make a real impact through AI, full-stack development, and data visualization. Always eager to learn and solve real-world problems with code.
+            I'm a Business Analyst from Melbourne, Australia with a knack for creating innovative solutions that make a real impact through AI, full-stack development, and data visualization. Always eager to learn and solve real-world problems.
           </div>
           <a
             href="mailto:archiechadha12@gmail.com"
